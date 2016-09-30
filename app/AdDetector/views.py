a28bfd2a42ce51190d8e60cba49526addb8ae964
Cory Stephenson <CStephenson970@gmail.com>
2016-09-29 22:33:34 -0700

    Fix a bug in the checkpage where a percentage was used as a probability

--- app/AdDetector/views.py
+++ app/AdDetector/views.py
@@ -83,9 +83,9 @@ def check_link():
             
     result = admodel.evaluate_text(article_text)
     ad_chance = round(result[1],3)*100
-    if ad_chance >= 0.5:
+    if ad_chance >= 50:
         response_str = "This looks like sponsored content!"
-    if ad_chance < 0.5:
+    if ad_chance < 50:
         response_str = "This looks like a regular article!"
         
     page_html = render_template("checkpage.html",response_str=response_str)
